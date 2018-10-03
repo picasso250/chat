811f003e1184c6b7d3df576fa96c7ce627eee7ef
picasso250 <cumt.xiaochi@gmail.com>
2018-10-03 23:31:52 +0800

    add GA

--- view/layout.php
+++ view/layout.php
@@ -5,7 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>不是微信的聊天</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-34649081-5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-34649081-5');
+</script>
 </head>
+
 <body>
     <div class="content-wrap">
         <?php include $_inner_; ?>
